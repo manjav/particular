8a2161aa2f0ab5a857fc3fd77f0a297a8195442a
Mansour <mansurjavadi@gmail.com>
2024-04-29 07:13:48 +0300

    cleanups - move inspector codes to class view

---
+++ editor/lib/display/inspector_view.dart
@@ -0,0 +1,274 @@
+import 'package:editor/data/inspector.dart';
+import 'package:editor/data/particular_editor_controller.dart';
+import 'package:editor/services/io.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intry/intry.dart';
+import 'package:particular/particular.dart';
+
+class InspactorView extends StatefulWidget {
+  final Map configs;
+  final ParticularEditorController controller;
+  const InspactorView({
+    super.key,
+    required this.configs,
+    required this.controller,
+  });
+
+  @override
+  State<InspactorView> createState() => _InspactorViewState();
+}
+
+class _InspactorViewState extends State<InspactorView> {
+  final _selectedColor = ValueNotifier<String?>(null);
+  int _selectedTabIndex = 0;
+
+  @override
+  void initState() {
+    _selectTab(_selectedTabIndex);
+    super.initState();
+  }
+
+  void _selectTab(int index) {
+    _selectedTabIndex = index;
+    var list = <Inspector>[];
+    for (var line in widget.configs["inspector"]["components"][index]) {
+      list.add(Inspector(
+        line["ui"] ?? "input",
+        line["min"],
+        line["max"],
+        line["type"],
+        line["title"] ?? "",
+        line["inputs"] ?? {},
+      ));
+    }
+    Inspector.list.value = list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var themeData = Theme.of(context);
+    return SizedBox(
+      width: widget.configs["inspector"]["width"],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _tabBarBuilder(),
+          _inspactorListBuilder(themeData),
+          const Expanded(child: SizedBox()),
+          _colorPickerBuilder(),
+        ],
+      ),
+    );
+  }
+
+  Widget _tabBarBuilder() {
+    return ValueListenableBuilder(
+      valueListenable: Inspector.list,
+      builder: (context, value, child) {
+        return Container(
+          color: Colors.black12,
+          child: Row(
+            children: [
+              _tabItemBuilder(0, Icons.snowing),
+              _tabItemBuilder(1, Icons.color_lens),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _tabItemBuilder(int index, IconData icon) {
+    return Expanded(
+      child: IconButton(
+        color:
+            _selectedTabIndex != index ? Theme.of(context).splashColor : null,
+        icon: Icon(icon),
+        onPressed: () => _selectTab(index),
+      ),
+    );
+  }
+
+  Widget _inspactorListBuilder(ThemeData themeData) {
+    return ValueListenableBuilder(
+      valueListenable: Inspector.list,
+      builder: (context, value, child) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              Text(
+                ["Emitter Settings", "Particle Settings"][_selectedTabIndex],
+              ),
+              const SizedBox(height: 16),
+              for (Inspector inspector in value)
+                _inspectorItemBuilder(themeData, inspector),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _inspectorItemBuilder(ThemeData themeData, Inspector inspector) {
+    if (inspector.type == null ||
+        inspector.type ==
+            [
+              "gravity",
+              "radial"
+            ][widget.controller.getParam("emitterType").index]) {
+      var items = <Widget>[];
+      _inputLineBuilder(
+        inspector,
+        items,
+        themeData,
+        (themeData, inspector, entry) =>
+            _addInputs(themeData, inspector, entry),
+      );
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          inspector.title.isEmpty
+              ? const SizedBox()
+              : _getText(inspector.title, themeData),
+          const SizedBox(height: 2),
+          Row(children: items),
+          const SizedBox(height: 6),
+        ],
+      );
+    }
+    return const SizedBox();
+  }
+
+  void _inputLineBuilder(
+    Inspector inspector,
+    List<Widget> children,
+    ThemeData themeData,
+    Widget Function(ThemeData themeData, Inspector inspector,
+            MapEntry<String, dynamic> entry)
+        inspectorBuilder,
+  ) {
+    for (var entry in inspector.inputs.entries) {
+      children.add(_getText(entry.key.toTitleCase(), themeData));
+      children.add(const SizedBox(width: 8));
+      children.add(
+        Expanded(
+          child: ListenableBuilder(
+            listenable: widget.controller.getNotifier(entry.value),
+            builder: (c, w) => inspectorBuilder(themeData, inspector, entry),
+          ),
+        ),
+      );
+      children.add(const SizedBox(width: 12));
+    }
+  }
+
+  Widget _addInputs(ThemeData themeData, Inspector inspector,
+      MapEntry<String, dynamic> entry) {
+    if (inspector.ui == "input") {
+      return NumericIntry(
+        slidingSpeed: 1,
+        min: inspector.min,
+        max: inspector.max,
+        decoration: NumericIntryDecoration.outline(context),
+        value: widget.controller.getParam(entry.value).toDouble(),
+        onChanged: (double value) => _updateParticleParam(entry.value, value),
+      );
+    } else if (inspector.ui == "dropdown") {
+      List values = switch (entry.value) {
+        "blendFunctionSource" ||
+        "blendFunctionDestination" =>
+          BlendFunction.values,
+        "renderBlendMode" || "textureBlendMode" => BlendMode.values,
+        _ => EmitterType.values,
+      };
+      var items = values
+          .map((item) => DropdownMenuItem(
+              value: item,
+              child: _getText(item.toString().toTitleCase(), themeData)))
+          .toList();
+      return InputDecorator(
+        decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.symmetric(horizontal: 8)),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton(
+            itemHeight: 48,
+            items: items,
+            value: widget.controller.getParam(entry.value),
+            onChanged: (dynamic selected) {
+              widget.controller.updateFromMap({entry.value: selected});
+              if (entry.value == "emitterType") {
+                setState(() {});
+              }
+            },
+          ),
+        ),
+      );
+    } else if (inspector.ui == "color") {
+      return IconButton(
+        icon: Icon(
+          Icons.circle,
+          color: widget.controller.getParam(entry.value).getColor(),
+        ),
+        onPressed: () => _selectedColor.value = entry.value,
+      );
+    } else {
+      // Button
+      return OutlinedButton(
+        onPressed: () async {
+          final image = await browseImage();
+          if (image != null) {
+            widget.controller.update(texture: image);
+          }
+        },
+        child: _getText("${entry.value}".toTitleCase(), themeData),
+      );
+    }
+  }
+
+  Text _getText(String text, ThemeData themeData) =>
+      Text(text, style: themeData.primaryTextTheme.labelSmall);
+
+  void _updateParticleParam(String key, num value) {
+    var param = widget.controller.getParam(key);
+    widget.controller
+        .updateFromMap({key: param is int ? value.toInt() : value});
+  }
+
+  Widget _colorPickerBuilder() {
+    return ValueListenableBuilder<String?>(
+      valueListenable: _selectedColor,
+      builder: (context, value, child) {
+        if (value == null) {
+          return const SizedBox();
+        }
+        return TapRegion(
+          onTapOutside: (event) => _selectedColor.value = null,
+          child: Container(
+            color: Colors.black12,
+            padding: const EdgeInsets.all(16),
+            child: SlidePicker(
+              showIndicator: false,
+              showSliderText: false,
+              pickerColor: widget.controller.getParam(value).getColor(),
+              onColorChanged: (color) {
+                widget.controller.updateFromMap({
+                  value: ARGB(color.alpha / 255, color.red / 255,
+                      color.green / 255, color.blue / 255)
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+extension StringExtension on String {
+  String toTitleCase() => "${this[0].toUpperCase()}${substring(1)}";
+}
--- editor/lib/main.dart
+++ editor/lib/main.dart
@@ -1,13 +1,10 @@
 import 'dart:convert';
 
 import 'package:editor/data/particular_editor_controller.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:editor/display/inspector_view.dart';
+import 'package:editor/services/io.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:image/image.dart' as image;
-import 'package:intry/intry.dart';
 import 'package:particular/particular.dart';
 
 void main() {
@@ -22,40 +19,21 @@ class EditorApp extends StatefulWidget {
 }
 
 class _EditorAppState extends State<EditorApp> {
-  // Add controller to change particle
-  final _particleController = ParticularEditorController();
-  final _selectedInspactorColumn = ValueNotifier([]);
-  final _selectedColor = ValueNotifier<String?>(null);
-  int _selectedTabIndex = 0;
   Map _appConfigs = {};
+  final _particleController = ParticularEditorController();
 
   @override
   void initState() {
-    _loadAppConfigs();
-    _loadDefaultTexture();
+    _loadInitialConfigs();
     super.initState();
   }
 
-  // Load configs data for app from json
-  _loadAppConfigs() async {
+  /// Loads the initial configurations for the application.
+  ///
+  /// This function does not have any parameters and does not return any value.
+  void _loadInitialConfigs() async {
     var json = await rootBundle.loadString("assets/app_configs.json");
     _appConfigs = Map.castFrom(jsonDecode(json));
-    setState(() {});
-  }
-
-  void _selectTab(int index) {
-    _selectedTabIndex = index;
-    var list = <Inspector>[];
-    for (var line in _appConfigs["inspector"]["components"][index]) {
-      list.add(Inspector(
-        line["ui"] ?? "input",
-        line["type"],
-        line["title"] ?? "",
-        line["inputs"] ?? {},
-      ));
-    }
-    _selectedInspactorColumn.value = list;
-  }
 
   /// Load default particle texture
     final ByteData assetImageByteData =
@@ -86,7 +64,10 @@ class _EditorAppState extends State<EditorApp> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             _canvasBuilder(),
-            _inspactorBuilder(),
+            InspactorView(
+              configs: _appConfigs,
+              controller: _particleController,
+            ),
           ],
         ),
       ),
@@ -140,225 +121,4 @@ class _EditorAppState extends State<EditorApp> {
       ),
     );
   }
-
-  Widget _inspactorBuilder() {
-    var themeData = Theme.of(context);
-    return Container(
-      width: 330,
-      color: themeData.colorScheme.inverseSurface,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _tabBarBuilder(),
-          _inspactorListBuilder(themeData),
-          const Expanded(child: SizedBox()),
-          _colorPickerBuilder(),
-        ],
-      ),
-    );
-  }
-
-  Widget _tabBarBuilder() {
-    return ValueListenableBuilder(
-      valueListenable: _selectedInspactorColumn,
-      builder: (context, value, child) {
-        return Container(
-          color: Colors.black12,
-          child: Row(
-            children: [
-              _tabItemBuilder(0, Icons.snowing),
-              _tabItemBuilder(1, Icons.color_lens),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _tabItemBuilder(int index, IconData icon) {
-    return Expanded(
-      child: IconButton(
-        color:
-            _selectedTabIndex != index ? Theme.of(context).splashColor : null,
-        icon: Icon(icon),
-        onPressed: () => _selectTab(index),
-      ),
-    );
-  }
-
-  Widget _inspactorListBuilder(ThemeData themeData) {
-    return ValueListenableBuilder(
-      valueListenable: _selectedInspactorColumn,
-      builder: (context, value, child) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              Text(
-                ["Emitter Settings", "Particle Settings"][_selectedTabIndex],
-              ),
-              const SizedBox(height: 16),
-              for (Inspector inspector in value)
-                _inspectorItemBuilder(themeData, inspector),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _inspectorItemBuilder(ThemeData themeData, Inspector inspector) {
-    if (inspector.type == null ||
-        inspector.type ==
-            [
-              "gravity",
-              "radial"
-            ][_particleController.getParam("emitterType").index]) {
-      var items = <Widget>[];
-      _inputLineBuilder(
-        inspector,
-        items,
-        themeData,
-        (themeData, inspector, entry) =>
-            _addInputs(themeData, inspector, entry),
-      );
-
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          inspector.title.isEmpty
-              ? const SizedBox()
-              : _getText(inspector.title, themeData),
-          const SizedBox(height: 2),
-          Row(children: items),
-          const SizedBox(height: 6),
-        ],
-      );
-    }
-    return const SizedBox();
-  }
-
-  void _inputLineBuilder(
-    Inspector inspector,
-    List<Widget> children,
-    ThemeData themeData,
-    Widget Function(ThemeData themeData, Inspector inspector,
-            MapEntry<String, dynamic> entry)
-        inspectorBuilder,
-  ) {
-    for (var entry in inspector.inputs.entries) {
-      children.add(_getText(entry.key.toTitleCase(), themeData));
-      children.add(const SizedBox(width: 8));
-      children.add(
-        Expanded(
-          child: ListenableBuilder(
-            listenable: _particleController.getNotifier(entry.value),
-            builder: (c, w) => inspectorBuilder(themeData, inspector, entry),
-          ),
-        ),
-      );
-      children.add(const SizedBox(width: 12));
-    }
-  }
-
-  Widget _addInputs(ThemeData themeData, Inspector inspector,
-      MapEntry<String, dynamic> entry) {
-    if (inspector.ui == "input") {
-      return NumericIntry(
-        changeSpeed: 1,
-        decoration: NumericIntryDecoration.outline(context),
-        value: _particleController.getParam(entry.value).toDouble(),
-        onChanged: (double value) => _updateParticleParam(entry.value, value),
-      );
-    } else if (inspector.ui == "dropdown") {
-      List values = switch (entry.value) {
-        "blendFunctionSource" ||
-        "blendFunctionDestination" =>
-          BlendFunction.values,
-        "renderBlendMode" || "textureBlendMode" => BlendMode.values,
-        _ => EmitterType.values,
-      };
-      var items = values
-          .map((item) => DropdownMenuItem(
-              value: item,
-              child: _getText(item.toString().toTitleCase(), themeData)))
-          .toList();
-      return InputDecorator(
-        decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            contentPadding: EdgeInsets.symmetric(horizontal: 8)),
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton(
-            itemHeight: 48,
-            items: items,
-            value: _particleController.getParam(entry.value),
-            onChanged: (dynamic selected) {
-              _particleController.updateFromMap({entry.value: selected});
-              if (entry.value == "emitterType") {
-                setState(() {});
-              }
-            },
-          ),
-        ),
-      );
-    } else if (inspector.ui == "color") {
-      return IconButton(
-        icon: Icon(
-          Icons.circle,
-          color: _particleController.getParam(entry.value).getColor(),
-        ),
-        onPressed: () => _selectedColor.value = entry.value,
-      );
-    } else {
-      // Button
-      return OutlinedButton(
-        onPressed: _browseTexture,
-        child: _getText("${entry.value}".toTitleCase(), themeData),
-      );
-    }
-  }
-
-  Text _getText(String text, ThemeData themeData) =>
-      Text(text, style: themeData.primaryTextTheme.labelSmall);
-
-  void _updateParticleParam(String key, num value) {
-    var param = _particleController.getParam(key);
-    _particleController
-        .updateFromMap({key: param is int ? value.toInt() : value});
-  }
-
-  Widget _colorPickerBuilder() {
-    return ValueListenableBuilder<String?>(
-      valueListenable: _selectedColor,
-      builder: (context, value, child) {
-        if (value == null) {
-          return const SizedBox();
-        }
-        return TapRegion(
-          onTapOutside: (event) => _selectedColor.value = null,
-          child: Container(
-            color: Colors.black12,
-            padding: const EdgeInsets.all(16),
-            child: SlidePicker(
-              showIndicator: false,
-              showSliderText: false,
-              pickerColor: _particleController.getParam(value).getColor(),
-              onColorChanged: (color) {
-                _particleController.updateFromMap({
-                  value: ARGB(color.alpha / 255, color.red / 255,
-                      color.green / 255, color.blue / 255)
-                });
-              },
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-}
-
-extension StringExtension on String {
-  String toTitleCase() => "${this[0].toUpperCase()}${substring(1)}";
 }
