f28c2a2dc7bb5df8a1b71cb9b1a1e26fcb7d5870
Mansour <mansurjavadi@gmail.com>
2024-07-31 11:54:57 +0300

    enhance - editor - retouch colors

--- editor/lib/theme/theme.dart
+++ editor/lib/theme/theme.dart
@@ -1,29 +1,48 @@
 import 'package:flutter/material.dart';
 
+class Themes {
+  static Color backgroundColor = const Color(0xFF2C2C2C);
+  static Color foregroundColor = const Color(0xFF9E9E9E);
+}
+
 ThemeData get customTheme => ThemeData(
       useMaterial3: true,
       brightness: Brightness.dark,
       primarySwatch: Colors.grey,
-      appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF2C2C2C),
+      appBarTheme: AppBarTheme(
+          backgroundColor: Themes.backgroundColor,
           titleTextStyle: TextStyle(
-              fontSize: 12, fontWeight: FontWeight.w600, color: Colors.grey)),
-      scaffoldBackgroundColor: const Color(0xFF2C2C2C),
-      textTheme: const TextTheme(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              color: Themes.foregroundColor)),
+      scaffoldBackgroundColor: Themes.backgroundColor,
+      textTheme: TextTheme(
         labelLarge: TextStyle(
-            fontSize: 12, fontWeight: FontWeight.w300, color: Colors.grey),
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+            color: Themes.foregroundColor),
         labelMedium: TextStyle(
-            fontSize: 10, fontWeight: FontWeight.w300, color: Colors.grey),
+            fontSize: 10,
+            fontWeight: FontWeight.w300,
+            color: Themes.foregroundColor),
         labelSmall: TextStyle(
-            fontSize: 8, fontWeight: FontWeight.w300, color: Colors.grey),
+            fontSize: 8,
+            fontWeight: FontWeight.w300,
+            color: Themes.foregroundColor),
         titleLarge: TextStyle(
-            fontSize: 12, fontWeight: FontWeight.w600, color: Colors.grey),
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            color: Themes.foregroundColor),
         titleMedium: TextStyle(
-            fontSize: 10, fontWeight: FontWeight.w600, color: Colors.grey),
+            fontSize: 10,
+            fontWeight: FontWeight.w600,
+            color: Themes.foregroundColor),
         titleSmall: TextStyle(
-            fontSize: 8, fontWeight: FontWeight.w600, color: Colors.grey),
-        bodyLarge: TextStyle(fontSize: 12),
-        bodyMedium: TextStyle(fontSize: 10),
-        bodySmall: TextStyle(fontSize: 9),
+            fontSize: 8,
+            fontWeight: FontWeight.w600,
+            color: Themes.foregroundColor),
+        bodyLarge: const TextStyle(fontSize: 12),
+        bodyMedium: const TextStyle(fontSize: 10),
+        bodySmall: const TextStyle(fontSize: 9),
       ),
     );
